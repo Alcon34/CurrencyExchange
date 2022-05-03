2cfffa9d4e8e8fac60263f5355f0fd71f1fa47db
AlconBLR <timaskov.den@gmail.com>
2022-05-03 19:54:01 +0300

    v.4

--- Cron/UpdateRate.php
+++ Cron/UpdateRate.php
@@ -2,23 +2,35 @@
 
 namespace Tsimashkou\CurrencyExchange\Cron;
 
-use Psr\Log\LoggerInterface;
-
 class UpdateRate
 {
-    protected LoggerInterface $logger;
+    public function execute() {
 
-    public function __construct(LoggerInterface $logger) {
-        $this->logger = $logger;
     }
 
-    /**
-     * Write to system.log
-     *
-     * @return void
-     */
-    public function execute() {
-        $this->logger->info('Cron Works');
+    public function update()
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.apilayer.com/fixer/{date}?symbols={symbols}&base={base}",
+            CURLOPT_HTTPHEADER => array(
+                "Content-Type: text/plain",
+                "apikey: oDu7rJ3O5L0FV7Ea7kH0Rxkn0H16QopB"
+            ),
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET"
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        //echo $response;
     }
 
 }
--- Setup/InstallData.php
+++ Setup/InstallData.php
@@ -17,31 +17,6 @@ class InstallData implements InstallDataInterface
      */
     public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
-
-
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://api.apilayer.com/fixer/{date}?symbols={symbols}&base={base}",
-            CURLOPT_HTTPHEADER => array(
-                "Content-Type: text/plain",
-                "apikey: oDu7rJ3O5L0FV7Ea7kH0Rxkn0H16QopB"
-            ),
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET"
-        ));
-
-        $response = curl_exec($curl);
-
-        curl_close($curl);
-        echo $response;
-
-
         $installer = $setup;
         $installer->startSetup();
 
