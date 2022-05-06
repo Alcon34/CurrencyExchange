464fe5ddccf24d3ce68093e17d243a92094ac07e
AlconBLR <timaskov.den@gmail.com>
2022-05-06 16:29:56 +0300

    v.5

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-
 namespace Tsimashkou\CurrencyExchange\Setup;
 
 use Magento\Framework\DB\Ddl\Table;
